b9f1840761ee1f3b4711f5834efc2907a89be32d
Dnicx <theerapat.nick@gmail.com>
2018-06-13 16:47:53 +0900

    ecs work fine

--- Assets/Script/playerAI.cs
+++ Assets/Script/playerAI.cs
@@ -17,25 +17,22 @@ public class playerAI : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
+	//FIRST PLAYER MODEL
 	void FixedUpdate () {
-		//playerCharacter.GetComponent<playerScript>().goUp();
+		AvoidantAI();
+	}
+
+	void AvoidantAI() {
 		if (playerCharacter == null) return;
-		// if (rTrigger.GetComponent<playerTrigger>().warn) playerCharacter.GetComponent<playerScript>().goLeft();
-		// else if (lTrigger.GetComponent<playerTrigger>().warn) playerCharacter.GetComponent<playerScript>().goRight();
-		// else if (rfTrigger.GetComponent<playerTrigger>().warn) playerCharacter.GetComponent<playerScript>().goUp();
-		// else if (lfTrigger.GetComponent<playerTrigger>().warn) playerCharacter.GetComponent<playerScript>().goUp();
-		// else if (mTrigger.GetComponent<playerTrigger>().warn) playerCharacter.GetComponent<playerScript>().goDown();
-		// else playerCharacter.GetComponent<playerScript>().stop();
 		playerCharacter.GetComponent<playerScript>().pressShift();
 		playerCharacter.GetComponent<playerScript>().direction = Observe.GetComponent<observeRange>().weight;
-		// Debug.Log(Observe.GetComponent<observeRange>().weight.magnitude);
-		// if (Observe.GetComponent<observeRange>().weight.magnitude > 4) {
-		// 	playerCharacter.GetComponent<playerScript>().liftShift();
-		// 	playerCharacter.GetComponent<playerScript>().direction = new Vector3(playerCharacter.GetComponent<playerScript>().direction.y, -playerCharacter.GetComponent<playerScript>().direction.x, 0);
-		// 	// Debug.Log("turn! + " + Observe.GetComponent<observeRange>().weight.magnitude);
-		// }
 		if (Observe.GetComponent<observeRange>().weight == Vector3.zero) 
 			playerCharacter.GetComponent<playerScript>().direction = -playerCharacter.GetComponent<Transform>().position;
 		Observe.GetComponent<observeRange>().clear();
 	}
+
+	void simulateAI() {
+		
+	}
+
 }
